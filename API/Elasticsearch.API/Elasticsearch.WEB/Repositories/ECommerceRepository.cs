36bca8522af981d584a88583c6f4d597bfd35287
cakiciozgur <cakiciozgur@outlook.com>
2023-10-21 15:01:38 +0300

    ECommerce search method added

---
+++ API/Elasticsearch.API/Elasticsearch.WEB/Repositories/ECommerceRepository.cs
@@ -0,0 +1,82 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
+using Elasticsearch.WEB.Models;
+using Elasticsearch.WEB.ViewModel;
+
+namespace Elasticsearch.WEB.Repositories
+{
+    public class ECommerceRepository
+    {
+        private readonly ElasticsearchClient _elasticsearchClient;
+        private const string indexName = "kibana_sample_data_ecommerce";
+
+        public ECommerceRepository(ElasticsearchClient client)
+        {
+            _elasticsearchClient = client;
+        }
+
+
+        public async Task<(List<ECommerce> list, long count)> SearchAsync(ECommerceSearchViewModel searchViewModel, int page, int pageSize)
+        {
+
+            // totalCount 100
+            //page 1 pagesize=10 1-10,
+            //page 2 pagesize=10 11-20,
+
+
+            List<Action<QueryDescriptor<ECommerce>>> listQuery = new();
+
+            if (!string.IsNullOrWhiteSpace(searchViewModel.Category))
+            {
+                Action<QueryDescriptor<ECommerce>> query = (q) => q.Match(m => m
+                                                .Field(f => f.Category)
+                                                .Query(searchViewModel.Category));
+                listQuery.Add(query);
+            }
+
+
+            if (!string.IsNullOrWhiteSpace(searchViewModel.CustomerFullName))
+            {
+                Action<QueryDescriptor<ECommerce>> query = (q) => q.Match(m => m
+                                                .Field(f => f.CustomerFullName)
+                                                .Query(searchViewModel.CustomerFullName));
+                listQuery.Add(query);
+            }
+
+            if (searchViewModel.OrderDateStart.HasValue)
+            {
+                Action<QueryDescriptor<ECommerce>> query = (q) => q.Range(r => r
+                                                                    .DateRange(dr => dr
+                                                                    .Field(f => f.OrderDate)
+                                                                    .Gte(searchViewModel.OrderDateStart)));
+                listQuery.Add(query);
+            }
+
+            if (searchViewModel.OrderDateEnd.HasValue)
+            {
+                Action<QueryDescriptor<ECommerce>> query = (q) => q.Range(r => r
+                                                                    .DateRange(dr => dr
+                                                                    .Field(f => f.OrderDate)
+                                                                    .Lte(searchViewModel.OrderDateEnd)));
+                listQuery.Add(query);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchViewModel.Gender))
+            {
+                Action<QueryDescriptor<ECommerce>> query = (q) => q.Term(t=> t.Field(f=> f.Gender).Value(searchViewModel.Gender));
+                listQuery.Add(query);
+            }
+
+            var pageFrom = (page - 1) * pageSize;
+
+            var result = await _elasticsearchClient.SearchAsync<ECommerce>(s => s.Index(indexName).Size(pageSize).From(pageFrom)
+            .Query(q => q
+                .Bool(b => b
+                    .Must(listQuery.ToArray()))));
+
+            foreach (var item in result.Hits) item.Source.Id = item.Id;
+
+            return (result.Documents.ToList(), result.Total);
+        }
+    }
+}
---
+++ API/Elasticsearch.API/Elasticsearch.WEB/ViewModel/ECommerceSearchViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Elasticsearch.WEB.ViewModel
+{
+    public class ECommerceSearchViewModel
+    {
+        [Display(Name ="Category")]
+        public string Category { get; set; } = null!;
+
+        [Display(Name = "Gender")]
+        public string Gender { get; set; } = null!;
+
+        [Display(Name = "Order Date Start")]
+        [DataType(DataType.Date)]
+        public DateTime? OrderDateStart { get; set; }
+
+        [Display(Name = "Order Date End")]
+        [DataType(DataType.Date)]
+        public DateTime? OrderDateEnd { get; set; }
+
+        [Display(Name = "CustomerFullName")]
+        public string CustomerFullName { get; set; } = null!;
+    }
+}
